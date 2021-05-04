ea4c78644dad1fb388d683c57fbd12735aaa0a5c
JeffDziad <jeffdziad@gmail.com>
2021-05-03 20:10:17 -0500

    Finished Quiz 6 Work

--- src/composition/Student.java
+++ src/composition/Student.java
@@ -1,14 +1,19 @@
 package composition;
 
-public class Student extends Address {
+public class Student {
 
     private String name;
     private int age;
+    private Address address;
 
     public Student(String streetAddress, int zipCode, String state, String phone, String name, int age) {
-        super(streetAddress, zipCode, state, phone);
+        this.address = new Address(streetAddress, zipCode, state, phone);
         this.name = name;
         this.age = age;
+    }
+
+    public Address getAddress() {
+        return address;
     }
 
     public int getAge() {
